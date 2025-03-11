b7432336efe07fbbd8bb86c9ec15ffa928cc23f2
hyeriimm <94698088+hyeriimm@users.noreply.github.com>
2025-03-11 17:25:34 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 547 ms, Memory: 60,196 KB -BaekjoonHub

---
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+	
+	private static int N;
+	private static int[] nums;
+	private static int change;
+	private static int maxMoney;
+	private static ArrayList<String> visited;
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+
+			/* 0. Initialize */
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			String input = st.nextToken();
+			N = input.length();
+			nums = new int[N];
+			change = Integer.parseInt(st.nextToken());
+			maxMoney = 0;
+			visited = new ArrayList<>();
+
+			/* 1. Get numbers */
+			for (int i = 0; i < N; i++) {
+				nums[i] = input.charAt(i) -'0';
+			}
+			
+			/* 2. Back Tracking */
+			dfs(0);
+
+			/* 3. Print result */
+			sb.append("#" + tc + " " + maxMoney + "\n");
+		}
+		System.out.println(sb);
+	}
+	
+	private static void dfs(int cnt) {
+		
+		if (cnt == change) {
+			int result = 0;
+			for (int i = 0; i < N; i++) {
+				result *= 10;
+				result += nums[i];
+			}
+			if (maxMoney < result) maxMoney = result;
+			return;
+		}
+		
+		// pruning
+		String key = Arrays.toString(nums) + " " + cnt;
+		if (visited.contains(key)) {
+			return;
+		}
+		visited.add(key);
+		
+		for (int i = 0; i < N - 1; i++) {
+			for (int j = i + 1; j < N ; j++) {
+				int temp = nums[i];
+				nums[i] = nums[j];
+				nums[j] = temp;
+				
+				dfs(cnt + 1);
+				
+				nums[j] = nums[i];
+				nums[i] = temp;
+			}
+		}
+	}
+}
