13939e0f474d5b2bea21e6129da0a0faee790197
hyeriimm <94698088+hyeriimm@users.noreply.github.com>
2025-03-21 10:30:57 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 2,205 ms, Memory: 102,516 KB -BaekjoonHub

--- SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -1,70 +1,78 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	
+
 	private static int N;
-	private static int[][] CC;
-	
-	/* Floyd-Warshall */
-	private static void floyd() {
-		
-		for (int k = 0; k < N; k++) {
+	private static int[][] adjMatrix;
+	private static boolean[] visited;
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+
+			/* 0. Initialize */
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			adjMatrix = new int[N][N];
+			int minCC = Integer.MAX_VALUE;
+
+			/* 1. Get adjacency matrix */
 			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) { // update
-					if (CC[i][k] + CC[k][j] < CC[i][j]) {
-						CC[i][j] = CC[i][k] + CC[k][j];
+				for (int j = 0; j < N; j++) {
+					adjMatrix[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+
+			/* 2. BFS */
+			for (int i = 0; i < N; i++) {
+				visited = new boolean[N];
+				int cci = bfs(i);
+				if (minCC > cci) minCC = cci;
+			}
+			
+			/* 3. Print result */
+			sb.append("#" + tc + " " + minCC + "\n");
+		}
+		System.out.println(sb);
+	}
+
+	private static int bfs(int start) {
+
+		Queue<Integer> queue = new ArrayDeque<>();
+		int level = 0;
+		int CC = 0;
+
+		queue.offer(start);
+		visited[start] = true;
+
+		while (!queue.isEmpty()) {
+
+			int size = queue.size();
+
+			for (int k = 0; k < size; k++) {
+				
+				int v = queue.poll();
+				CC += level;
+
+				for (int w = 0; w < N; w++) {
+					if (!visited[w] && adjMatrix[v][w] == 1) {
+						queue.offer(w);
+						visited[w] =true;
 					}
 				}
 			}
+			level++;
 		}
-	}
-	
-	private static int getMinCC() {
-		int minCC = Integer.MAX_VALUE;
-		for (int i = 0; i < N; i++) {
-			int sum = 0;
-			for (int j = 0; j < N; j++) {
-				sum += CC[i][j];
-			}
-			minCC = (minCC > sum) ? sum : minCC;
-		}
-		return minCC;
-	}
-
-	public static void main(String[] args) throws Exception {
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for (int tc = 1; tc <= T; tc++) {
-			
-			/* 0. Initialize */
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			N = Integer.parseInt(st.nextToken());
-			CC = new int[N][N];
-			
-			/* 1. Adjacency matrix */
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					int weight = Integer.parseInt(st.nextToken());
-					if (weight > 0)
-						CC[i][j] = weight;
-					else 
-						CC[i][j] = 999999;
-				}
-				CC[i][i] = 0;
-			}
-
-			/* 2. Floyd-Warshall */
-			floyd();
-			
-			/* 3. Print result */
-			sb.append("#" + tc + " " + getMinCC() + "\n");
-		}
-		System.out.println(sb);
+		return CC;
 	}
 }
