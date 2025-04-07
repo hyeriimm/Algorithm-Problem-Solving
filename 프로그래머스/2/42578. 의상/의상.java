f55d5c7cfcc248c68bb9bcbd246bf32f53d36e8f
hyeriimm <94698088+hyeriimm@users.noreply.github.com>
2025-04-07 19:58:28 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+	    
+        for (int i = 0; i < clothes.length; i++) {
+	       	if (map.containsKey(clothes[i][1])) {
+	       		map.replace(clothes[i][1], map.get(clothes[i][1]) + 1);
+        	}
+        	else {
+        		map.put(clothes[i][1], 1);
+        	}
+	    }
+        
+        for (int v : map.values()) {
+            answer *= (v + 1);
+        }
+        
+        return answer - 1;
+    }
+}
