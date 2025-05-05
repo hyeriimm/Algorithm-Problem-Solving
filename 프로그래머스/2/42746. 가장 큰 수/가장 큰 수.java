be9d3120c6e07fdf9e457428a10f3b5e0019a86f
hyeriimm <94698088+hyeriimm@users.noreply.github.com>
2025-05-06 03:39:24 +0900

    [level 2] Title: 가장 큰 수, Time: 232.29 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -4,12 +4,11 @@ import java.io.*;;
 class Solution {
     public String solution(int[] numbers) {
 
-        String[] nums = new String[numbers.length];
         StringBuilder sb = new StringBuilder();
         
-        for (int i = 0; i < numbers.length; i++) {
-            nums[i] = String.valueOf(numbers[i]);
-        }
+        String[] nums = Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .toArray(String[]::new);
         
         Arrays.sort(nums, (a, b) -> (b + a).compareTo(a + b));
         
